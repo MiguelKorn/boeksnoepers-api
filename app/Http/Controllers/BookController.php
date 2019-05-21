8ee96093cd09aa74e28e74840f1b32dcc43baa48
Miguel Korn <miguel.korn@hva.nl>
2019-05-21 14:17:38 +0200

    added questions and location data to books

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -31,7 +31,7 @@ class BookController extends Controller
 
     public function showWithRead($id)
     {
-        $books = Book::all();
+        $books = Book::with('questions', 'locations')->get();
 
         foreach ( $books as $book ) {
             $book->user = UserBook::where( 'book_id', $book->id )->where( 'user_id', $id )->where( 'competition_id', 2 )->get();
