79643492550cf08c2c6acc8c210ee849e609be17
Miguel Korn <miguel.korn@hva.nl>
2019-06-10 10:31:54 +0200

    change temp debug3

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -10,7 +10,7 @@ class QuestionController extends Controller
     public function store(Request $request)
     {
         $data = $request->json()->all();
-        return response()->json(['all'=>$request->all(), 'json'=>$data]);
+        return response()->json(['all'=>$request->all(), 'json'=>$data, 'content'=>$request->getContent()]);
 //        return response()->json( [ 'success' => ( Question::insert($data.data.questions) ) ], 200 );
     }
 }
