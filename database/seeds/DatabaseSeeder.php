8c281c6d8f20e79ab8804be3dead5203fa20fc84
Miguel Korn <miguel.korn@hva.nl>
2019-06-10 12:55:31 +0200

    chnged seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -118,7 +118,7 @@ class DatabaseSeeder extends Seeder
             [ 'id' => 2, 'user_id' => 2, 'book_id' => 4, 'competition_id' => 1, 'is_current' => false, 'score' => 3 ],
             [ 'id' => 3, 'user_id' => 2, 'book_id' => 1, 'competition_id' => 2, 'is_current' => true, 'score' => null ],
             [ 'id' => 4, 'user_id' => 3, 'book_id' => 2, 'competition_id' => 2, 'is_current' => false, 'score' => 5 ],
-            [ 'id' => 5, 'user_id' => 2, 'book_id' => 2, 'competition_id' => 2, 'is_current' => false, 'score' => 3 ],
+            [ 'id' => 5, 'user_id' => 2, 'book_id' => 3, 'competition_id' => 2, 'is_current' => false, 'score' => 3 ],
             [ 'id' => 6, 'user_id' => 2, 'book_id' => 2, 'competition_id' => 3, 'is_current' => true, 'score' => null ],
             [ 'id' => 7, 'user_id' => 3, 'book_id' => 2, 'competition_id' => 3, 'is_current' => false, 'score' => 5 ],
             [ 'id' => 8, 'user_id' => 5, 'book_id' => 2, 'competition_id' => 3, 'is_current' => false, 'score' => 4 ],
