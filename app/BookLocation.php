6145a83fffedea91eaf4ad875858c77d981f326c
Miguel Korn <miguel.korn@hva.nl>
2019-06-10 12:45:16 +0200

    changed store locations to booklocations

---
+++ app/BookLocation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BookLocation extends Model
+{
+    protected $table = 'book_location';
+
+}
--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Location;
+use App\BookLocation;
 
 class LocationController extends Controller
 {
     public function store(Request $request)
     {
         $locations = json_decode($request->input('locations'), true);
-        return response()->json( [ 'success' => ( Location::insert($locations) ) ], 200 );
+        return response()->json( [ 'success' => ( BookLocation::insert($locations) ) ], 200 );
     }
 }
